f410a8cf17a021139f73c97302e3af136ec6ea64
Cristian Soncco <cristian_soncco@msn.com>
2023-09-10 16:04:44 -0500

    Cambiar bootstrapApplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   // templateUrl: './app.component.html',
-  template: `<h1>Hello world!</h1>`,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
   title = 'homes';
+
 }
